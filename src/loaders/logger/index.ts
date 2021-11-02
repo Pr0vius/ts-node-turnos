41ffa442126d79ad7710c87d60184f5379b6649f
Braian Delgado <braiandelgado4255@hotmail.com>
2021-11-02 16:34:29 -0300

    log file for production, command for producction and .envexample added

--- src/loaders/logger/index.ts
+++ src/loaders/logger/index.ts
@@ -3,6 +3,11 @@ import config from "../../config";
 const transports = [];
 
 transports.push(new winston.transports.Console());
+if (config.env === "production") {
+  transports.push(
+    new winston.transports.File({ filename: "Errors.log", level: "Error" })
+  );
+}
 
 const logger = winston.createLogger({
   level: config.log.level,
