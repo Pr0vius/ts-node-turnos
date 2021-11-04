e0301f63f7f77086cacaad03e6d1ca41a0d5f2bb
Braian Delgado <braiandelgado4255@hotmail.com>
2021-11-03 21:16:28 -0300

    message added

--- src/helpers/SuccessResponse.ts
+++ src/helpers/SuccessResponse.ts
@@ -1,8 +1,10 @@
 export class SuccessResponse {
   code: number;
   data: any;
+  message:string;
   constructor(code: number, data: any) {
     this.code = code;
+    this.message = 'Ok'
     this.data = data;
   }
 }
