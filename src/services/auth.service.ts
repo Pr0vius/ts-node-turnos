6a932869c1df22520447e2ac55aae56ce359f3dc
Braian Delgado <braiandelgado4255@hotmail.com>
2021-11-04 17:48:24 -0300

    singIn method created

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,9 +3,10 @@ import bcrypt from "bcrypt";
 import config from "../config";
 import { IUser, IUserLogin } from "../models/interfaces";
 import userRepo from "../repository/User";
+import { ErrorResponse } from "../helpers/ErrorResponse";
 
 export class AuthService {
-  private encrypt(id: string | number) {
+  private encrypt(id: string | number | undefined) {
     return jwt.sign({ id }, config.jwt.secret, {
       expiresIn: config.jwt.expires,
     });
@@ -22,7 +23,22 @@ export class AuthService {
     };
   }
   async singIn({ email, password }: IUserLogin) {
-    return await userRepo.findUser({ email, password });
+    const user = await userRepo.findUserByEmail(email);
+    if (!user) {
+      throw new ErrorResponse(400, undefined, "Email or password are wrong");
+    }
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      throw new ErrorResponse(400, undefined, "Email or password are wrong");
+    }
+    const token = this.encrypt(user._id);
+    return {
+      id: user._id,
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      token,
+    };
   }
 }
 export default new AuthService();
