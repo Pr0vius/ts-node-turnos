5cacdee1a52ae9748c0f5129a0f7ba191cc81289
Braian Delgado <braiandelgado4255@hotmail.com>
2021-11-04 17:48:44 -0300

    singIn tests

--- src/test/auth.test.ts
+++ src/test/auth.test.ts
@@ -4,14 +4,20 @@ import User from "../models/UserSchema";
 import config from "../config";
 
 const { prefix } = config;
+let connection: any;
+beforeAll(async () => {
+  connection = await connectDatabase();
+});
+afterEach(async () => {
+  await User.deleteMany();
+});
+afterAll(() => {
+  connection.disconnect();
+});
 
 describe("POST AUTH/REGISTER: ", () => {
-  let connection: any;
-  beforeAll(async () => {
-    connection = await connectDatabase();
-  });
   test("headers", async () => {
-    const response = await api
+    await api
       .post(`${prefix}/register`)
       .send(initialUsers[0])
       .expect("Content-Type", /application\/json/)
@@ -24,10 +30,30 @@ describe("POST AUTH/REGISTER: ", () => {
     expect(response.body.data.email).toContain("John@Doe.com");
     expect(response.body.data.password).toBeUndefined();
   });
-  afterEach(async () => {
-    await User.deleteMany();
+});
+
+describe("POST AUTH/LOGIN", () => {
+  test("headers", async () => {
+    await api.post(`${prefix}/register`).send(initialUsers[0]);
+    await api
+      .post(`${prefix}/login`)
+      .send({
+        email: initialUsers[0].email,
+        password: initialUsers[0].password,
+      })
+      .expect("Content-Type", /application\/json/)
+      .expect(200);
   });
-  afterAll(() => {
-    connection.disconnect();
+  test("body", async () => {
+    await api.post(`${prefix}/register`).send(initialUsers[0]);
+    const response = await api.post(`${prefix}/login`).send({
+      email: initialUsers[0].email,
+      password: initialUsers[0].password,
+    });
+    expect(response.body.message).toBe("Ok");
+    expect(response.body.data.username).toContain("theman69");
+    expect(response.body.data.name).toContain("John Doe");
+    expect(response.body.data.email).toContain("John@Doe.com");
+    expect(response.body.data.password).toBeUndefined();
   });
 });
