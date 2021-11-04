56ae6cd4221af47d7c4e17a28f80532260ada703
Braian Delgado <braiandelgado4255@hotmail.com>
2021-11-03 21:34:25 -0300

    email & name added

--- src/models/UserSchema.ts
+++ src/models/UserSchema.ts
@@ -2,7 +2,9 @@ import { Schema, Model, model } from "mongoose";
 import { IUser } from "./interfaces";
 
 const userSchema = new Schema<IUser, Model<IUser>, IUser>({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  name: { type: String },
   password: { type: String, required: true },
   company: [{ type: Schema.Types.ObjectId, ref: "Company" }],
 });
