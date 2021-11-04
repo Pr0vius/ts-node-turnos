c32686f5871299eda8dce82d225ebf5b10a8e8c0
Braian Delgado <braiandelgado4255@hotmail.com>
2021-11-03 21:38:30 -0300

    encrypted password added

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,11 +1,21 @@
-import { UserRepository } from "../repository/User";
-const repository = new UserRepository();
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import config from "../config";
+import { IUser, IUserLogin } from "../models/interfaces";
+import userRepo from "../repository/User";
 
-export class AuthService{
-  async singUp(body: object){
-    return await repository.createUser(body);
-  };
-  async singIn() {
-    return await repository.findUser();
+export class AuthService {
+  private encrypt(id: string | number) {
+    return jwt.sign({ id }, config.jwt.secret, {
+      expiresIn: config.jwt.expires,
+    });
+  }
+  async singUp(user: IUser) {
+    user.password = await bcrypt.hash(user.password, 10);
+    return await userRepo.createUser(user);
+  }
+  async singIn({ email, password }: IUserLogin) {
+    return await userRepo.findUser({ email, password });
   }
 }
+export default new AuthService();
