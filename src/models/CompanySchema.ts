f1839860cf95f8efceabe442827b25c7925ff9df
Braian Delgado <braiandelgado4255@hotmail.com>
2021-11-03 21:34:54 -0300

    squema added

--- src/models/CompanySchema.ts
+++ src/models/CompanySchema.ts
@@ -2,9 +2,9 @@ import { Schema, Model, model } from "mongoose";
 import { ICompany } from "./interfaces";
 
 const companySchema = new Schema<ICompany, Model<ICompany>, ICompany>({
-  name: { type: String, required: true },
-  dates: [{ type: Date, required: true }],
-  users: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  name: { type: String, required: true, unique: true },
+  turns: [{ type: Schema.Types.ObjectId, ref: "Turn" }],
+  users: [{ type: Schema.Types.ObjectId, ref: "User", unique: true }],
 });
 
 export default model<ICompany>("Company", companySchema);
