5a26d653a5ca47e96b65b3cb3169a5d5d8b8e68a
Braian Delgado <braiandelgado4255@hotmail.com>
2021-11-08 21:30:18 -0300

    Create company validations

--- src/middlewares/validations/company.ts
+++ src/middlewares/validations/company.ts
@@ -1,4 +1,12 @@
+import { check } from "express-validator";
 import { validResult } from "./funtions/commons";
-import { validateJWT } from "./funtions/custom";
+import { validateJWT, companyNameIsUniqueValidator } from "./funtions/custom";
+
+const nameIsUnique = check("name")
+  .notEmpty()
+  .withMessage("The name of the company is required")
+  .custom(companyNameIsUniqueValidator)
+  .withMessage("The comapny name already exist");
 
 export const getCompaniesValidator = [validateJWT, validResult];
+export const postCompanyValidation = [validateJWT, nameIsUnique, validResult];
