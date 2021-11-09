816763f6339464c360080296af14c2dc535de2e7
Braian Delgado <braiandelgado4255@hotmail.com>
2021-11-08 21:32:08 -0300

    Create company method created

--- src/controllers/company.controllers.ts
+++ src/controllers/company.controllers.ts
@@ -1,7 +1,7 @@
 import { Request as Req, Response as Res, NextFunction as Next } from "express";
 import { ErrorResponse } from "../helpers/ErrorResponse";
 import { SuccessResponse as Success } from "../helpers/SuccessResponse";
-
+import companyService from "../services/company.service";
 export const getAllCompanies = async (req: Req, res: Res, next: Next) => {
   try {
     console.log(req.user);
@@ -10,6 +10,19 @@ export const getAllCompanies = async (req: Req, res: Res, next: Next) => {
     throw new ErrorResponse(
       err.code || 500,
       err.message || "Couldn't get company list",
+      err.data || "Something went wrong"
+    );
+  }
+};
+export const createCompany = async (req: Req, res: Res, next: Next) => {
+  try {
+    const company = { name: req.body.name };
+    const newCompany = await companyService.create(req.user._id, company);
+    res.status(201).json(new Success(200, newCompany));
+  } catch (err: any) {
+    throw new ErrorResponse(
+      err.code || 500,
+      err.message || "Couldn't create the company",
       err.data || "Something went wrong"
     );
   }
