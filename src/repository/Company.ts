3816106ffb7e6b54c942cf5bac8f52bf14c9912f
Braian Delgado <braiandelgado4255@hotmail.com>
2021-11-10 12:34:20 -0300

    fin by id method added

--- src/repository/Company.ts
+++ src/repository/Company.ts
@@ -2,14 +2,18 @@ import Company from "../models/CompanySchema";
 import { ICompany } from "../models/interfaces";
 class CompanyRepository {
   async getCompanyList() {
-    return await Company.find();
+    return await Company.find().populate('users');
   }
   async createNewCompany(body: ICompany) {
     return await Company.create(body);
   }
   async findByName(name: string) {
-    return await Company.findOne({ name });
+    return await Company.findOne({ name }).populate("users");
   }
+  async findById(id: string){
+    return await Company.findById(id).populate('users');
+
+  };
 }
 
 export default new CompanyRepository();
